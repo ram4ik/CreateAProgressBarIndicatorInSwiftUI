67db33a3d511cda1efd207967b09af591a1c9395
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-05 22:18:53 +0200

    init

--- CreateAProgressBarIndicatorInSwiftUI/ContentView.swift
+++ CreateAProgressBarIndicatorInSwiftUI/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progressAmount = 0.0
+    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        Text("Hello, world!")
+        ProgressView("Progress...", value: progressAmount, total: 100)
+            .accentColor(.green)
             .padding()
+            .onReceive(timer, perform: { _ in
+                if progressAmount < 100 {
+                    progressAmount += 2
+                }
+            })
     }
 }
 
